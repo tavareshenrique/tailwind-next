d162957ed95fb20ea13216f33066705949cde299
Henrique Tavares <ihenrits@gmail.com>
2024-04-03 07:39:54 -0300

    feat: animating select

--- src/components/Form/Select/index.tsx
+++ src/components/Form/Select/index.tsx
@@ -27,7 +27,7 @@ export function Select({ children, placeholder, ...props }: ISelectProps) {
           side="bottom"
           position="popper"
           sideOffset={8}
-          className="z-10 w-[--radix-select-trigger-width] overflow-hidden rounded-lg border border-zinc-200 bg-white"
+          className="animate-slideDownAndFade z-10 w-[--radix-select-trigger-width] overflow-hidden rounded-lg border border-zinc-200 bg-white"
         >
           <SelectPrimitive.Viewport>{children}</SelectPrimitive.Viewport>
         </SelectPrimitive.Content>
