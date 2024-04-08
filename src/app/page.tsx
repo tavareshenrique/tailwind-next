0b1cba1bd8a43ad95ec2cade7051e562fc0fd298
Henrique Tavares <ihenrits@gmail.com>
2024-04-08 08:31:32 -0300

    refactor: create button variants

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Bold, Italic, Link, List, ListOrdered, Mail } from 'lucide-react'
 
+import { Button } from '@/components/Button'
 import * as FileInput from '@/components/Form/FileInput'
 import { Select } from '@/components/Form/Select'
 import { SelectItem } from '@/components/Form/Select/SelectItem'
@@ -24,19 +25,12 @@ export default function Home() {
           </div>
 
           <div className="flex items-center gap-2">
-            <button
-              type="button"
-              className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
-            >
+            <Button type="button" variant="outline">
               Cancel
-            </button>
-            <button
-              type="button"
-              className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
-              form="settings"
-            >
+            </Button>
+            <Button type="button" form="settings">
               Save
-            </button>
+            </Button>
           </div>
         </div>
 
@@ -229,18 +223,12 @@ export default function Home() {
           </div>
 
           <div className="flex items-center justify-end gap-2 pt-5">
-            <button
-              type="button"
-              className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
-            >
+            <Button type="button" variant="outline">
               Cancel
-            </button>
-            <button
-              type="button"
-              className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
-            >
+            </Button>
+            <Button type="button" form="settings">
               Save
-            </button>
+            </Button>
           </div>
         </form>
       </div>
