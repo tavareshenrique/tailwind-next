d06ca49356ada229f60a50667682686c299e9c7f
Henrique Tavares <ihenrits@gmail.com>
2024-04-09 08:02:30 -0300

    refactor: responsiviness for tabs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,10 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en" className="antialiased">
       <body className={inter.className}>
-        <div className="grid min-h-screen grid-cols-app bg-white ">
+        <div className="min-h-screen bg-white lg:grid lg:grid-cols-app ">
           <Sidebar />
 
-          <main className="px-4 pb-12 pt-24 lg:col-start-2 lg:px-8 lg:pt-8">
+          <main className="max-w-[100vw] px-4 pb-12 pt-24 lg:col-start-2 lg:px-8 lg:pt-8">
             {children}
           </main>
         </div>
--- src/components/SettingsTabs/TabItem.tsx
+++ src/components/SettingsTabs/TabItem.tsx
@@ -13,7 +13,7 @@ export function TabItem({ title, value, isSelected = false }: ITabItemProps) {
       className="group relative cursor-pointer px-1 pb-4 text-sm font-medium text-zinc-500 outline-none transition-colors duration-200 hover:text-violet-700 data-[state=active]:text-violet-700"
       value={value}
     >
-      <span className="rounded group-focus-visible:ring-2 group-focus-visible:ring-violet-400 group-focus-visible:ring-offset-4">
+      <span className="whitespace-nowrap rounded group-focus-visible:ring-2 group-focus-visible:ring-violet-400 group-focus-visible:ring-offset-4">
         {title}
       </span>
 
